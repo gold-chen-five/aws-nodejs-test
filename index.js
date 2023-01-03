7f27dae56e54f31f76f891971e2e544d528dc4ca
phalanity <phalanity2@gmail.com>
2023-01-03 18:47:41 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+ 
+var server = app.listen(port, function () {
+    console.log(`open server at port ${port}`)
+})
